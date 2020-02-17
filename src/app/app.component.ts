1eaeedc8b7ed864d0e43019d09dd7d72304555af
rouzbeh.majlessi@avanade.com <rouzbeh.majlessi@avanade.com>
2020-02-17 15:27:29 -0800

    add mock data to constructor and populate table data with it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngfor-angular';
+  dataList: any[];
+  constructor() {
+    this.dataList = [
+      {
+        'FirstName' : 'Roy',
+        'LastName'  : 'Agasthyan'
+      },
+      {
+        'FirstName' : 'Sam',
+        'LastName'  : 'Johnson'
+      },
+      {
+        'FirstName' : 'Anjali',
+        'LastName'  : 'Sharma'
+      },
+      {
+        'FirstName' : 'Julia',
+        'LastName'  : 'Sherwood'
+      }
+    ];
+  }
 }
